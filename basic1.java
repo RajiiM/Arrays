8b1098af66b0048ea0639c9d0fbe5cf2511e52db
M.Rajalakshmi <mrajalakshmiofficial@gmail.com>
2021-12-22 18:17:54 +0530

    Updated

---
+++ basic1.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		
+		String frnds[] = {"balaji", "munees", "gowtham"};
+		Arrays.sort(frnds);
+		for(String name:frnds)
+		{
+			System.out.println(name);
+		}
+		
+		int number[] = {10,15,6,9,8,2,0};
+		Arrays.sort(number);
+		for(int no:number)
+		{
+			System.out.println(no);
+		}
+	}
+}
+---------------------------------
+OUTPUT:
+balaji
+gowtham
+munees
+0
+2
+6
+8
+9
+10
+15
+===============================================
+================================================
